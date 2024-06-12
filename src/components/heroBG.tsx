d6d16136f208455ba4351f73188e1bfee4379b95
Ali Moallim <amoallim15@gmail.com>
2024-06-12 12:20:13 +0300

    improved bg

---
+++ src/components/heroBG.tsx
@@ -0,0 +1,35 @@
+export default function () {
+  return (
+    <>
+      <svg
+        className="absolute inset-0 z-[-10] h-full w-full stroke-white opacity-20 mesh"
+        aria-hidden="true"
+      >
+        <defs>
+          <pattern
+            id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
+            width="200"
+            height="200"
+            x="50%"
+            y="-1"
+            patternUnits="userSpaceOnUse"
+          >
+            <path d="M.5 200V.5H200" fill="none"></path>
+          </pattern>
+        </defs>
+        <svg x="50%" y="-1" className="overflow-visible aop">
+          <path
+            d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+            strokeWidth="0"
+          ></path>
+        </svg>
+        <rect
+          width="100%"
+          height="100%"
+          strokeWidth="0"
+          fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"
+        ></rect>
+      </svg>
+    </>
+  );
+}
