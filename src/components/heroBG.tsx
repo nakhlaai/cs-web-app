7ffae81719f067faf12492a12e87d4c98e2c33c6
Ali Moallim <amoallim15@gmail.com>
2024-06-12 17:17:53 +0300

    updated

--- src/components/heroBG.tsx
+++ src/components/heroBG.tsx
@@ -2,7 +2,7 @@ export default function () {
   return (
     <>
       <svg
-        className="absolute inset-0 z-[-10] h-full w-full stroke-white opacity-20 mesh"
+        className="absolute inset-0 z-[-10] h-full w-full stroke-white/10 ltr:[mask-image:radial-gradient(1000%_100%_at_top_right,white,transparent)] rtl:[mask-image:radial-gradient(100%_100%_at_top_left,white,transparent)]"
         aria-hidden="true"
       >
         <defs>
@@ -17,7 +17,7 @@ export default function () {
             <path d="M.5 200V.5H200" fill="none"></path>
           </pattern>
         </defs>
-        <svg x="50%" y="-1" className="overflow-visible aop">
+        <svg x="50%" y="-1" className="overflow-visible fill-gray-800/20">
           <path
             d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
             strokeWidth="0"
